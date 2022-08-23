d34082f5663209a329adea7f1602a552f7b7082c
SHANMUKHA Gopagoni <67545133+shanmukhzayn1@users.noreply.github.com>
2022-08-23 15:08:22 +0530

    Update converting-time.py

--- converting-time.py
+++ converting-time.py
@@ -1,11 +1,11 @@
 #user input seconds 
-user_seconds = input("please enter number of seconds") 
+user_seconds = input("please enter number of seconds you wish to convertt") 
 num_seconds =  int(user_seconds) 
 #calculating the number of hourse 
 num_hourse = num_seconds//3600 
 num_seconds_left = num_seconds % 3600 
 #calculating the number of minutes 
 num_minutes = num_seconds_left // 60 
-final_seconds = num_minutes % 60 
+final_seconds = num_seconds_left % 60 
 print("hourse=", num_hourse , "minutes = ", num_minutes, "seconds =", final_seconds) 
 
